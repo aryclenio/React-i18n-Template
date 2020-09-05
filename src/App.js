3afb56492f9a6de490e9b2bf1f1b72448bc2ac91
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-09-04 21:47:26 -0300

    :lipstick: Adding internationalization

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("pt")}>PT</button>
+      <hr />
+      <div><h1>{t("title")}</h1></div>
+      <div>{t("description.part1")}</div>
+      <div>{t("description.part2")}</div>
+    </div>
+  );
+}
+
+export default App;
